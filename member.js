b8effbc3facd1e05e22b2bed662916f928d243ef
Nori Hayashi <net@hayashi-lin.net>
2023-09-28 03:49:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var addSkills = document.getElementById("addSkills");
+    var removeSkills = document.getElementById("removeSkills");
+
+    skillsMember.style.display = "block";
+    addSkills.style.display = "none";
+    removeSkills.style.display = "none";
+    skills.style.display = "none";
+}
